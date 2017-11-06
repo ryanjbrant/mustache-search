458574c2b3295d636127c367f15a31399cab47dc
Ryan Brant <ryanjbrant@gmail.com>
2017-11-06 12:40:53 -0800

    Simplifying code so people can help
    asdf

--- js/main.js
+++ js/main.js
@@ -43,42 +43,7 @@ $("#searchText, #searchField").on("change paste keyup", function() {
 									<h4 class="search-tour">{{tour}}</h2>\
 								</div>\
 							</div>\
-                            <div class="description description--grid">\
-                                <h6 class="item-sub">{{tour}}</h6>\
-                                <h3 class="item-heading">{{artist}}</h3>\
-                                <div class="details">\
-	                                <ul>\
-	                                    <li onclick="FWDRL.show(\'{{lbid}}colRadio\', 0);"><div class="cat-icon"><img class="mic" src="images/icons8-Microphone-100-white.png"></div></li>\
-	                                    <li onclick="FWDRL.show(\'{{lbid}}colTV\', 0);"><div class="cat-icon"><img class="monitor" src="images/icons8-Monitor-100-white.png"></div></li>\
-	                                    <li onclick="FWDRL.show(\'{{lbid}}colArt\', 0);"><div class="cat-icon"><img class="resolution" src="images/icons8-Resolution-100-white.png"></div></li>\
-	                                </ul>\
-                                </div>\
-                                <div class="item-list">\
-                                    <ul>\
-                                        <li>\
-                                            <ul id="{{lbid}}colRadio" class="flex-col">\
-                                            {{#audio}}\
-                                                <li id="audio{{type}}" onclick="FWDRL.show(\'{{lbid}}colRadio\', {{index}});" data-url="{{url}}"  data-thumbnail-path="{{cover}}" data-width="600"><h4><i class="material-icons">mic</i> {{type}}</h4></li>\
-                                            {{/audio}}\
-                                            </ul>\
-                                        </li>\
-                                        <li>\
-                                            <ul id="{{lbid}}colTV" class="flex-col">\
-                                            {{#video}}\
-                                                <li id="video{{type}}" onclick="FWDRL.show(\'{{lbid}}colTV\', {{index}});" data-url="{{url}}" data-poster-path="{{cover}}" data-thumbnail-path="{{cover}}" data-width="800" data-height="450"><h4><i class="material-icons">play_circle_outline</i> {{type}}</h4><p class="description"></p></li>\
-                                            {{/video}}\
-                                            </ul>\
-                                        </li>\
-                                        <li>\
-                                            <ul id="{{lbid}}colArt" class="flex-col">\
-                                            {{#artwork}}\
-                                                <li id="artwork{{type}}" onclick="FWDRL.show(\'{{lbid}}colArt\', {{index}});" data-url="{{url}}" data-thumbnail-path="{{url}}"><h4><i class="material-icons">create</i> {{type}}</h4></li>\
-                                            {{/artwork}}\
-                                            </ul>\
-                                        </li>\
-                                    </ul>\
-                                </div>\
-                            </div>\
+                            <div class="description description--grid">{{Name}}</div>\
                         </a>\
 					</div>\
 					{{ /. }}';
